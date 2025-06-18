9fc3849f584df2e124d16cbdf1923046c81a95f4
riteshpandile1 <ritesh.pandile@portalwiz.com>
2025-06-18 16:46:48 +0530

    latest commit

--- lib/views/widgets/stockcard.dart
+++ lib/views/widgets/stockcard.dart
@@ -6,34 +6,30 @@ class StockCard extends StatelessWidget {
   final StockModel stock;
   const StockCard({Key? key, required this.stock}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      child: ListTile(
-        title: Text(
-          stock.symbol,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(stock.sector),
-        trailing: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              '\$${stock.price.toStringAsFixed(2)}',
-              style: const TextStyle(fontSize: 16),
-            ),
-           
-            Text(
-              '${stock.change >= 0 ? '+' : ''}${stock.change.toStringAsFixed(2)}%',
-              style: TextStyle(
-                color: stock.change >= 0 ? Colors.green : Colors.red,
-                fontSize: 13,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      child: Card(
+        color: Colors.white,
+       shape: RoundedRectangleBorder(side: BorderSide(color: Colors.blue.shade100, width: 1.0),
+       borderRadius: BorderRadius.circular(12),),
+       elevation: 2,
+        child: ListTile(
+          title: Text("${stock.symbol}"),
+          subtitle: Text("${stock.sector}"),
+          trailing: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text("\$ ${stock.price}", style: TextStyle(fontSize: 16)),
+        
+              Text(
+                " ${stock.change >= 0 ? '+' : ''}${stock.change.toStringAsFixed(2)}%",
+                style: TextStyle(fontSize: 16, color: stock.change >= 0 ? Colors.green : Colors.red),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
